5888b83980b7d57a50e63769ba433f468776f0e5
alainritchie <alain.ritchie@thisismatrix.com>
2021-01-08 17:37:22 +0000

    initial changes to content and styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -26,10 +26,10 @@ const Header = () => {
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            {title || 'Hello. My name is'}{' '}
+            <span className="text-color-main">{name || 'Alain Ritchie'}</span>
             <br />
-            {subtitle || "I'm the Unknown Developer."}
+            {subtitle || "I'm a technical problem solver."}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
--- src/mock/data.js
+++ src/mock/data.js
@@ -2,26 +2,28 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
+  title: 'Alain Ritchie | Problem Solver', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hello. My name is',
+  name: 'Alain Ritchie',
+  subtitle: "I'm an experienced problem solver",
+  cta: 'Tell me more',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'headshot.jpg',
+  paragraphOne:
+    'I am an entrepreneurially motivated individual with more than 15 years of technology, software, business and data development experience, specialising in the generation of commercial value, through the design and implementation of bespoke solutions, and optimising business efficiency.',
+  paragraphTwo:
+    'I am tenacious and dedicated in character, with history of delivering innovative and successful solutions to problems in corporate, SME and lean start-up environments.',
+  paragraphThree: 'The ability to communicate complex subjects with a relevent level of simplicity means I am at home working with people at all levels of experience and knowledge.',
+  resume: 'https://www.linkedin.com/in/alainritchie/',
 };
 
 // PROJECTS DATA
@@ -57,8 +59,8 @@ export const projectsData = [
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
+  cta: 'Want to see if we can work together? I do.',
+  btn: 'Get in Touch',
   email: '',
 };
 
@@ -67,28 +69,13 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/alainritchie/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/MatrixAlain',
     },
   ],
-};
-
-// Github start/fork buttons
-export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
 };
