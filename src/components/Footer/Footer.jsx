6061470483d96e864b1bd0abb220b71575e9fcaa
alainritchie <alain.ritchie@thisismatrix.com>
2021-01-18 12:19:36 +0000

    added google analyics

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -32,6 +32,7 @@ const Footer = () => {
               );
             })}
         </div>
+        <div><a class="small-link" href="javascript:gaOptout();">Deactive Google Tracking</a></div>
         <hr />
       </Container>
     </footer>
